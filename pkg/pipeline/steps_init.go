9cde6cebe309bb840889b2adf3da4d2c7ecefff4
Leif Johansson <leifj@sunet.se>
2026-04-05 23:58:39 +0200

    style: format steps_init.go

--- pkg/pipeline/steps_init.go
+++ pkg/pipeline/steps_init.go
@@ -3,14 +3,14 @@ package pipeline
 
 func init() {
 	// Unified steps (auto-detect format based on content/extension/structure)
-	RegisterFunction("load", Load)       // Auto-detect TSL (XML) or LoTE (JSON)
-	RegisterFunction("publish", Publish) // Publish all trust lists in context
+	RegisterFunction("load", Load)         // Auto-detect TSL (XML) or LoTE (JSON)
+	RegisterFunction("publish", Publish)   // Publish all trust lists in context
 	RegisterFunction("generate", Generate) // Auto-detect from directory structure
 	RegisterFunction("merge", Merge)       // Merge all trust lists of same type
 
 	// Explicit TSL (ETSI TS 119 612) steps
 	RegisterFunction("load-tsl", LoadTSL)
-	RegisterFunction("load-xml", LoadTSL)       // Alias
+	RegisterFunction("load-xml", LoadTSL) // Alias
 	RegisterFunction("publish-tsl", PublishTSL)
 	RegisterFunction("publish-xml", PublishTSL) // Alias
 	RegisterFunction("generate-tsl", GenerateTSL)
@@ -18,7 +18,7 @@ func init() {
 
 	// Explicit LoTE (ETSI TS 119 602) steps
 	RegisterFunction("load-lote", LoadLoTE)
-	RegisterFunction("load-json", LoadLoTE)       // Alias
+	RegisterFunction("load-json", LoadLoTE) // Alias
 	RegisterFunction("publish-lote", PublishLoTE)
 	RegisterFunction("publish-json", PublishLoTE) // Alias
 	RegisterFunction("generate-lote", GenerateLoTE)
